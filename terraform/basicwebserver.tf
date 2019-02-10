a4c6d9150f3d6ed00cc1cefde8f9408292cff987
David Neal <35005080+davidpneal@users.noreply.github.com>
2019-02-10 11:59:25 -0600

    Add code to generate the Security Group needed by the EC2 instance

--- terraform/basicwebserver.tf
+++ terraform/basicwebserver.tf
@@ -1,10 +1,12 @@
 #2/10/2019
-#This config requires the keypair and security groups to already exist in AWS
+#This config requires the keypair to already exist in AWS
+#This version of the config will also create a basic Security Group
 
 
-#Variables
+#Variables definined in terraform.tfvars
 variable "private_key_path" {}
 variable "keypair_name" {}
+variable "public_ip" {}
 
 
 #AWS Provider
@@ -14,11 +16,46 @@ provider "aws" {
 
 
 #Resources
+
+#Import the default VPC so it can be managed by Terraform
+resource "aws_default_vpc" "DefaultVPC" {}
+
+#Security Group
+resource "aws_security_group" "WebServerSG" {
+  name        = "WebServerSG"
+  vpc_id      = "${aws_default_vpc.DefaultVPC.id}"
+
+  # SSH access from a whitelisted address
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.public_ip}"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Instance
 resource "aws_instance" "webserver01" {
   ami           = "ami-035be7bafff33b6b6"
   instance_type = "t2.micro"
   key_name      = "${var.keypair_name}"
-  vpc_security_group_ids = ["WebDMZ"]
+  vpc_security_group_ids = ["${aws_security_group.WebServerSG.id}"]
 
   connection {
     user        = "ec2-user"
@@ -35,7 +72,6 @@ resource "aws_instance" "webserver01" {
     ]
   }
 }
-
 
 
 #Output
