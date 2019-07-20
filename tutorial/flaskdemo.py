818d53daba5de1dc35bec59e79fcf1f7ba49d9a8
David Neal <35005080+davidpneal@users.noreply.github.com>
2019-07-20 16:32:43 -0500

    Add registration and login forms to the flask application to allow user input
    The Flask extension wtforms has some classes that help build forms.  Add a new forms.py file that sets up 2 new form classes - these classes are converted to html when run.
    Add new routes to the main file for the Registration and Login pages.  Add some methods to these routes to handle the POST method
    Add 2 new templates for the Registration and Login pages.  The HTML in the tutorial uses some bootstrap style sheet stuff, however I am trying to omit all of that to keep it as simple as possible for now.
    Add data validation checks and feedback to indicate why a validation fails - this is handled inside of the template pages (register and login)
    Add a flash message to indicate when a user account has been created successfully - right now this just means the user filled out the registration page correctly
    Add a secret key to help secure the forms from cross-site request forgery attacks.  For now this key is part of the code, but later needs to be changed to something more secure since posting the key to GitHub makes this insecure…
    Add a simple header to ease navigation among the pages

--- tutorial/flaskdemo.py
+++ tutorial/flaskdemo.py
@@ -1,9 +1,14 @@
-#7/16/2019
-from flask import Flask, render_template
+#7/20/2019
+from flask import Flask, render_template, url_for, flash, redirect
+
+#Import the forms we created in the forms.py file
+from forms import RegistrationForm, LoginForm
 
 #Instantiate the Flask class
 app = Flask(__name__)
 
+#Need a secret key to validate our forms (need a better way to handle this secret later on)
+app.config['SECRET_KEY'] = '1d6ad6c2c107847c9ee3900cfdb9b88d'
 
 #Add some dummy data to work with for the demo
 #This list represents the data a db call would return when queried
@@ -23,7 +28,6 @@ posts_list = [
 ]
 
 
-#Add some basic view functions
 @app.route("/")
 @app.route("/index")
 def index():
@@ -33,9 +37,33 @@ def index():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    #If the data validates OK, redirect back to the index page
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!')
+        return redirect(url_for('index'))
+
+    return render_template('register.html', title='Registration', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    #Add some validation code, we dont have a user db so we will dummy it up for now
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Please check username and password.')
+
+    return render_template('login.html', title='Login', form=form)
+
 
 #If run from the command line with python script.py, this will run flask to stand up a temp dev webserver
 #Want to run this application in debug mode so changes are visible without restarting the www server
 if __name__ == '__main__':
     app.run(debug=True)
-    
