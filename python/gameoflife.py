9db30eb063c7b36901f84d8f7bd529d16958d6d3
David Neal <35005080+davidpneal@users.noreply.github.com>
2020-12-02 17:00:38 -0600

    Merge everything into the class

--- python/gameoflife.py
+++ python/gameoflife.py
@@ -8,45 +8,51 @@
 #Use Numpy ?
 
 
+class gameoflife(object):
+ 
+    def __init__(self, gridsize):
+        #Create the matrix and populate it with cells
+        self.grid[] = some_matrix_class(gridsize)
 
-#TODO: Check this syntax - just guessing here
-#self -- prob need to use this in the methods
+        for each row in grid:
+            for each col in grid:
+                self.grid[row,col] = True / False #DEFAULT VALUES?
 
-class cell(object):
-    
-    #Class variables \ state
-    state
-    state_next
-    posx
-    posy    
+                #Later on - have a way to init a specific pattern into the matrix (set specific cells to live)
 
-    #Can I pass a '1' through the call with the built in init method, or will I need a separate method: ie: cell.init(1) - marks it live, 0 or no input = dead
-    def __init__(self, row, col, state):
-        posx = row
-        posy = col
-        #If state was passed, store it
-        state = state
-        ##ELSE -- if NO var is passed, set a default of 0
-    
-    # step - calculate and return the next generation based on current state
+
     def step():
-        #decide: should the calc_neighbors code go in here?
-        n = calc_neighbors() #Is this right way to re-pass data?
-        
-        #Logic to calculate the next generation state
-        #Check this syntax
-        if n <= 1:
-            state_next = False #Dies from loneliness
-        elif n = 2:
-            state_next = state #2 neighbors = no change
-        elif n = 3:
-            state_next = True #Will always return true since dead cells become live with 3 neighbors
-        elif n >= 4:
-            state_next = False #Dies from overcrowding
+        #Calculate the number of the neighbors for each cell in the matrix
+        self.num_neighbors[] = some_matrix_class(len(grid))
+
+        for each row in grid:
+            for each col in grid:
+                self.num_neighbors[row,col] = self.grid[row,col].calc_neighbors()
+            
+        #Use the num_neighbors matrix to calc next state for each cell, can directly update the state
+        for each row in grid:
+            for each col in grid:
+                #Logic to calculate the next generation state
+                #Check this syntax
+                if num_neighbors[row,col] <= 1:
+                    self.grid[row,col] = False  #Dies from loneliness
+                elif num_neighbors[row,col] = 2:
+                    pass                        #Two neighbors = no change
+                elif num_neighbors[row,col] = 3:
+                    self.grid[row,col] = True   #Will always return true since dead cells become live with 3 neighbors
+                elif num_neighbors[row,col] >= 4:
+                    seld.grid[row,col] = False  #Dies from overcrowding
+
+
+        #Can have some code to detect no life - break the while loop and/or reset the board and start over
+
+
+    def state():
+        return self.grid
 
 
     def calc_neighbors():
-        neighbors = 0
+        self.neighbors = 0
 
         #Need some edge case handling - either looping code, or an invisible edge row
 
@@ -56,56 +62,34 @@ class cell(object):
                 if grid[x][y].state == True
                     neighbors = neighbors + 1
         
-        return neighbors
-
-
-    def save(): 
-        state = state_next
+        return self.neighbors
 
 
 
 
 
 
-steps = 0 #zero = infinite steps (later auto detect when all life dies), or an int value
-gridsize = 30,30
-
-#This makes grid global data -- is there a better way to handle this?
-#Later on - have a way to init a specific pattern into the matrix (set specific cells to live)
-grid = ?.generate(gridsize), add an instance of the cell class to each location
-for each row in grid:
-    for each col in grid:
-        grid[pos].init()
 
 
+if __name__ == '__main__':
 
-#This seems sloppy - is there a way to not loop this grid so much?
+    gridsize = 100,100
 
-iteration = 0
-while steps = 0 or iteration != steps:
-    #step all cells in the grid
-    for each row in grid:
-        for each col in grid:
-            grid[pos].step()
+    life = gameoflife(gridsize)
+    
+    steps = 0
+    iteration = 0
+    while steps = 0 or iteration != steps:
+        life.step()
+        draw(life.state())
 
-    #save the next generation's state as the current state
-    for each row in grid:
-        for each col in grid:
-            grid[pos].save()
-
-    #draw the matrix to screen
-    for each row in grid:
-        for each col in grid:
-            state = grid[pos].state #test this syntax
-            #draw this T/F value to the screen - pygame?
-
-    iteration = iteration + 1
-
-    #Can have some code to detect no life - break the while loop and/or reset the board and start over
+        iteration = iteration + 1
 
 
 
-##OTHER NOTES
-#The class needs to be able to read info from dataset / matrix
+
+
+
+#OTHER NOTES
 #Later - how about an 'infinite' grid -- no set size, it grows as needed (how fast will this be though)
 
